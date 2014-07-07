6a11e9ab271dcd73c80813360aa65a709ebe35b0
Bernard Mordan <bernardmordan@gmail.com>
2014-07-07 23:01:10 +0100

    awesome maths

--- fingerping.js
+++ fingerping.js
@@ -1,46 +1,8 @@
 if (Meteor.isClient) {
 
-  function step(e,vx,dir){
+  function decay(e){
     
     var x = e.currentTarget.children.markerO.offsetLeft;
-    
-    if(dir === 'stop'){
-      setTimeout(function(){
-        e.currentTarget.children.markerO.style.left = '0px';
-        e.currentTarget.children.markerO.style.top = e.target.clientHeight/2 + 'px';
-        return;
-      },vx);   
-    }else if(dir === 'pos'){
-      if(x < e.currentTarget.offsetWidth+30){
-      
-        var nx = x += 5;
-        e.currentTarget.children.markerO.style.left = nx + 'px';
-        setTimeout(function(){
-          step(e,vx,dir)
-        },vx);
-        
-      }else{
-        
-        dir = 'neg';
-        step(e,vx,dir);
-        
-      }
-    }else if(dir === 'neg'){
-      if(x > 20){
-      
-        var nx = x -= 5;
-        e.currentTarget.children.markerO.style.left = nx + 'px';
-        setTimeout(function(){
-          step(e,vx,dir)
-        },vx);
-        
-      }else{
-        
-        dir = 'pos';
-        step(e,vx,dir);
-        
-      }
-    }
     
   };//end of step
 
@@ -50,39 +12,55 @@ if (Meteor.isClient) {
     
     var delta = $('#delta');
     var el = this.find('#pad');
-    
-    Hammer(el).on('swipe', function(e){
-      e.gesture.preventDefault();
-
-        var ox = e.gesture.center.pageX;
-        var oy = e.gesture.center.pageY;  
-        var dx = e.gesture.deltaX;
-        var dy = e.gesture.deltaY;
-        var a = e.gesture.angle;
-        var vx = e.gesture.velocityX;
-        var vy = e.gesture.velocityX;
-        if(vx === 0){
-          vx = 0.1;
-        }
-        
-        if(dx > 0){
-          var dir = 'pos';
-          e.currentTarget.children.markerO.style.left = e.currentTarget.children.markerO.offsetLeft + 'px';
-          e.currentTarget.children.markerO.style.top = e.target.clientHeight/2 + 'px';
-        }else{
-          var dir = 'neg';
-          e.currentTarget.children.markerO.style.left = e.currentTarget.children.markerO.offsetLeft + 'px';
-          e.currentTarget.children.markerO.style.top = e.target.clientHeight/2 + 'px';
-        }
-
-      //step triggers animation  
-      step(e,vx,dir);
       
+    Hammer(el).on('touch drag', function(e){
+      e.gesture.preventDefault();
+      e.currentTarget.children.markerO.style.left = e.gesture.touches[0].pageX-10 + 'px';
+      e.currentTarget.children.markerO.style.top = e.gesture.touches[0].pageY-10 + 'px';  
     });
     
-    Hammer(el).on('doubletap', function(e){
-      e.gesture.preventDefault();
-      step(e,100,'stop');
+    Hammer(el).on('dragend', function(e){
+      console.log(e);
+      var ox = e.gesture.touches[0].pageX-10;
+      var oy = e.gesture.touches[0].pageY-10;
+      var a = e.gesture.angle;
+      var vx = e.gesture.velocityX;
+      var vy = e.gesture.velocityY;
+      var dir = e.gesture.direction;
+      var cw = e.currentTarget.clientWidth;
+      var ch = e.currentTarget.clientHeight;
+      if(a < 0 && dir === 'right'){
+        
+        delta[0].innerHTML = '+y+x ';
+        delta[0].innerHTML += (ox-cw)*-1;
+        delta[0].innerHTML += ',';
+        delta[0].innerHTML += ((ch-oy)-ch)*-1;
+      
+      }else if(a < 0 && dir === 'left'){
+        
+        delta[0].innerHTML = '+y-x ';
+        delta[0].innerHTML += ((cw-ox)-cw)*-1;
+        delta[0].innerHTML += ',';
+        delta[0].innerHTML += ((ch-oy)-ch)*-1;
+      
+      }else if(a > 0 && dir === 'right'){
+        
+        delta[0].innerHTML = '-y+x ';
+        delta[0].innerHTML += (ox-cw)*-1;
+        delta[0].innerHTML += ',';
+        delta[0].innerHTML += ch-oy;
+      
+      }else if(a > 0 && dir === 'left'){
+        
+        delta[0].innerHTML = '-y-x ';
+        delta[0].innerHTML += ((cw-ox)-cw)*-1;
+        delta[0].innerHTML += ',';
+        delta[0].innerHTML += (oy-ch)*-1;
+      
+      }
+      e.target.children.markerO.style.left = ox + 'px';
+      e.target.children.markerO.style.top = oy + 'px';
+      
     });
     
   };
