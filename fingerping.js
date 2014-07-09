9fdb14327d56a647417ec2fc7781be10d7cc1f18
Bernard Mordan <bernardmordan@gmail.com>
2014-07-09 22:42:11 +0100

    closer to movement, but no movement

--- fingerping.js
+++ fingerping.js
@@ -20,46 +20,63 @@ if (Meteor.isClient) {
     });
     
     Hammer(el).on('dragend', function(e){
+      
+      function moveUpRight(e){
+        var v = ((e.gesture.velocityX+e.gesture.velocityY)/2)*100;
+        var a = Math.round(e.gesture.angle)*-1;
+        var clientWidthSplitRight = e.currentTarget.clientWidth - e.currentTarget.children.markerO.offsetLeft;
+        var clientWidthSplitLeft = e.currentTarget.children.markerO.offsetLeft;
+        var movex = v/clientWidthSplitLeft;
+        
+        if(e.currentTarget.children.markerO.offsetLeft > e.currentTarget.clientWidth){
+          return;
+        }else{
+          console.log(a);
+          console.log(clientWidthSplitRight+'|'+movex);
+          console.log(e.currentTarget.children.markerO.style.left);
+          console.log(e.currentTarget.children.markerO.offsetLeft);
+          console.log(v);
+        }
+      };
+      
       console.log(e);
       var ox = e.gesture.touches[0].pageX-10;
       var oy = e.gesture.touches[0].pageY-10;
-      var a = e.gesture.angle;
-      var vx = e.gesture.velocityX;
-      var vy = e.gesture.velocityY;
+      var a = Math.round(e.gesture.angle);
+      var v = (e.gesture.velocityX+e.gesture.velocityX)*10;
       var dir = e.gesture.direction;
       var cw = e.currentTarget.clientWidth;
       var ch = e.currentTarget.clientHeight;
       if(a < 0 && dir === 'right'){
         
-        delta[0].innerHTML = '+y+x ';
-        delta[0].innerHTML += (ox-cw)*-1;
-        delta[0].innerHTML += ',';
-        delta[0].innerHTML += ((ch-oy)-ch)*-1;
+        delta[0].innerHTML = '+x+y ';
+        var X = cw - ox;
+        var Y = Math.tan(a)*X; Y = Y*-1;
+        moveUpRight(e);
       
       }else if(a < 0 && dir === 'left'){
         
-        delta[0].innerHTML = '+y-x ';
-        delta[0].innerHTML += ((cw-ox)-cw)*-1;
-        delta[0].innerHTML += ',';
-        delta[0].innerHTML += ((ch-oy)-ch)*-1;
+        delta[0].innerHTML = '-x+y ';
+        delta[0].innerHTML += ox;
+
       
       }else if(a > 0 && dir === 'right'){
         
-        delta[0].innerHTML = '-y+x ';
-        delta[0].innerHTML += (ox-cw)*-1;
-        delta[0].innerHTML += ',';
-        delta[0].innerHTML += ch-oy;
+        delta[0].innerHTML = '+x-y ';
+        delta[0].innerHTML += ox;
+
       
       }else if(a > 0 && dir === 'left'){
         
-        delta[0].innerHTML = '-y-x ';
-        delta[0].innerHTML += ((cw-ox)-cw)*-1;
-        delta[0].innerHTML += ',';
-        delta[0].innerHTML += (oy-ch)*-1;
+        delta[0].innerHTML = '-x-y ';
+        delta[0].innerHTML += ox;
+
       
       }
-      e.target.children.markerO.style.left = ox + 'px';
-      e.target.children.markerO.style.top = oy + 'px';
+      e.currentTarget.children.markerO.style.left = ox + 'px';
+      e.currentTarget.children.markerO.style.top = oy + 'px';
+      
+      
       
     });
     
