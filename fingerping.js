1f2c67e5e27499014096815583b58accac18b9d6
Bernard Mordan <bernardmordan@gmail.com>
2014-07-12 11:31:55 +0100

    third base. animation fires ok. cant stop it nicely

--- fingerping.js
+++ fingerping.js
@@ -1,12 +1,81 @@
 if (Meteor.isClient) {
 
-  function decay(e){
+  function move(e,v,dir){
     
     var x = e.currentTarget.children.markerO.offsetLeft;
+    var y = e.currentTarget.children.markerO.offsetTop;
+    var dv = 60;
+    switch(dir){
+      case 'rightup':
+        if(x > e.currentTarget.clientWidth && y > 0){
+          e.currentTarget.children.markerO.style.left = (x-v) + 'px';
+          e.currentTarget.children.markerO.style.top = (y-v) + 'px';
+          setTimeout(function(){move(e,v,'leftup');},dv);
+        }else if(y < 0 && x < e.currentTarget.clientWidth){
+          e.currentTarget.children.markerO.style.left = (x+v) + 'px';
+          e.currentTarget.children.markerO.style.top = (y+v) + 'px';
+          setTimeout(function(){move(e,v,'rightdown');},dv);
+        }else{
+          e.currentTarget.children.markerO.style.left = (x+v) + 'px';
+          e.currentTarget.children.markerO.style.top = (y-v) + 'px';
+          setTimeout(function(){move(e,v,'rightup');},dv);
+        }
+        break;
+      case 'rightdown':
+        if(x > e.currentTarget.clientWidth && y > 0){
+          e.currentTarget.children.markerO.style.left = (x-v) + 'px';
+          e.currentTarget.children.markerO.style.top = (y+v) + 'px';
+          setTimeout(function(){move(e,v,'leftdown');},dv);
+        }else if(y > e.currentTarget.clientHeight && x < e.currentTarget.clientWidth){
+          e.currentTarget.children.markerO.style.left = (x+v) + 'px';
+          e.currentTarget.children.markerO.style.top = (y-v) + 'px';
+          setTimeout(function(){move(e,v,'rightup');},dv);
+        }else{
+          e.currentTarget.children.markerO.style.left = (x+v) + 'px';
+          e.currentTarget.children.markerO.style.top = (y+v) + 'px';
+          setTimeout(function(){move(e,v,'rightdown');},dv);
+        }
+        break;
+      case 'leftup':
+        if(x < 0 && y > 0){
+          e.currentTarget.children.markerO.style.left = (x+v) + 'px';
+          e.currentTarget.children.markerO.style.top = (y-v) + 'px';
+          setTimeout(function(){move(e,v,'rightup');},dv);
+        }else if(y < 0 && x > 0){
+          e.currentTarget.children.markerO.style.left = (x-v) + 'px';
+          e.currentTarget.children.markerO.style.top = (y+v) + 'px';
+          setTimeout(function(){move(e,v,'leftdown');},dv);
+        }else{
+          e.currentTarget.children.markerO.style.left = (x-v) + 'px';
+          e.currentTarget.children.markerO.style.top = (y-v) + 'px';
+          setTimeout(function(){move(e,v,'leftup');},dv);
+        }
+        break;
+      case 'leftdown':
+        if(x < 0 && y > 0){
+          e.currentTarget.children.markerO.style.left = (x+v) + 'px';
+          e.currentTarget.children.markerO.style.top = (y+v) + 'px';
+          setTimeout(function(){move(e,v,'rightdown');},dv);
+        }else if(y > e.currentTarget.clientHeight && x > 0){
+          e.currentTarget.children.markerO.style.left = (x-v) + 'px';
+          e.currentTarget.children.markerO.style.top = (y-v) + 'px';
+          setTimeout(function(){move(e,v,'leftup');},dv);
+        }else{
+          e.currentTarget.children.markerO.style.left = (x-v) + 'px';
+          e.currentTarget.children.markerO.style.top = (y+v) + 'px';
+          setTimeout(function(){move(e,v,'leftdown');},dv);
+        }
+        break;
+      case 'stop':
+        var x = e.currentTarget.clientWidth / 2;
+        var y = e.currentTarget.clientHeight / 2;
+        e.currentTarget.children.markerO.style.left = (x-10) + 'px';
+        e.currentTarget.children.markerO.style.top = (y-10) + 'px';
+        return;
+        break;       
+    }
     
-  };//end of step
-
-
+  };
 
   Template.pad.rendered = function(template){
     
@@ -21,50 +90,31 @@ if (Meteor.isClient) {
     
     Hammer(el).on('dragend', function(e){
       
-      function move(e){
-        var vx = Math.round(e.gesture.velocityX*100);
-        var vy = Math.round(e.gesture.velocityY*100);
-        e.currentTarget.children.markerO.style.left += vx + 'px';
-        e.currentTarget.children.markerO.style.top += vy + 'px';
-      };
       
-      //console.log(e);
       var a = Math.round(e.gesture.angle);
       var dir = e.gesture.direction;
-      var w = e.currentTarget.clientWidth;
-      var h = e.currentTarget.clientHeight;
-      var ox = e.currentTarget.children.markerO.offsetLeft;
-      var oy = e.currentTarget.children.markerO.offsetTop;
-      var vx = Math.round(e.gesture.velocityX*100);
-      var vy = Math.round(e.gesture.velocityY*100);
-      var dt = e.timeStamp
-      //visual deltas
-      
+      var v = ((e.gesture.velocityX+e.gesture.velocityY)/2)*100;
       
       if(a < 0 && dir === 'right'){
-        
-        delta[0].innerHTML = '+x+y ';
-        move(e);
-       
+        move(e,v,'rightup');
       }else if(a < 0 && dir === 'left'){
-        
-        delta[0].innerHTML = '-x+y ';
-  
+        move(e,v,'leftup');  
       }else if(a > 0 && dir === 'right'){
-        
-        delta[0].innerHTML = '+x-y ';
-        
+        move(e,v,'rightdown');
       }else if(a > 0 && dir === 'left'){
-        
-        delta[0].innerHTML = '-x-y ';
-      
+        move(e,v,'leftdown');
       }
       
-      e.currentTarget.children.delta.innerHTML += 'ox:'+ox+' oy:'+oy+' vx:'+vx+' vy:'+vy+' w:'+w+' h:'+h;
+      //e.currentTarget.children.delta.innerHTML = 'ox:'+ox+' oy:'+oy+' v:'+v+' a:'+a+' vx:'+vx+' vy:'+vy+' w:'+w+' h:'+h;
       
     });
     
-  };
+    Hammer(el).on('swipe', function(e){
+      move(e,0,'stop');
+    });
+    
+  }
+    
 }
 
 if (Meteor.isServer) {
